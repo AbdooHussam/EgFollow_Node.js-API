a8b49993e9ae98ca9b5759289e64b001ca6bbad9
AbdooHussam <Booodyhussamh@gmail.com>
2023-10-13 19:27:05 +0300

    fix

--- src/routers/users_router.js
+++ src/routers/users_router.js
@@ -274,7 +274,10 @@ router.post("/userAddFriend", authMiddlewareUser, async (req, res) => {
     if (friendIndex != -1) {
       return res
         .status(404)
-        .send({ error: true, data: "This account has already been added" });
+        .send({
+          error: true,
+          data: `${user.following[friendIndex].username} has already been added`,
+        });
     }
 
     const response = await login_controller.addFriendship(
